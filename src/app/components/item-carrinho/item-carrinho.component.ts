f4b9cf927cc84c7c22d8d08bc3ed8e2d43527fa1
felipe <felipecleones999@gmail.com>
2024-03-14 18:31:34 -0300

    componentes&services separados

---
+++ src/app/components/item-carrinho/item-carrinho.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-carrinho',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-carrinho.component.html',
+  styleUrl: './item-carrinho.component.scss'
+})
+export class ItemCarrinhoComponent {
+
+}
