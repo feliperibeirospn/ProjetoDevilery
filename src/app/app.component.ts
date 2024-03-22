47f4c85bb2db1a3eb946deba36eedae8bc4ef0fa
felipe <felipecleones999@gmail.com>
2024-03-21 21:52:07 -0300

    carrin bugado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet,RouterModule } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './components/cart/cart.component';
+import { NavComponent } from './components/nav/nav.component';
+import { NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     ProductListComponent,
     FormsModule,
+    CartComponent,
     CommonModule,
-    CartComponent],
+    NavComponent, NgbPaginationModule,NgbAlertModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NavComponent } from './components/nav/nav.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  // Redirect to home if path is empty
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // Define route for home component
+  { path: 'home', component: HomeComponent },
+  // Define route for cart component
+  { path: 'cart', component: CartComponent },
+  // Define route for product list component (assuming it's the full path)
+  { path: 'products', component: ProductListComponent }, // Changed path to 'products'
+  // Wildcard route for 404 page (any other path)
+  { path: '**', component: PageNotFoundComponent },
+
+  { path: 'navbar', component: NavComponent}
+];
+
+export { routes };
