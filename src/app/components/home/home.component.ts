cec0a9ce0e69e8f37d5eeea32581c29867ace40d
felipe <felipecleones999@gmail.com>
2024-04-16 20:43:32 -0300

    ajustes layout e add total a msg

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit  } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  finalizar: boolean = false;
+  private cartSubscription: Subscription | undefined = undefined;
 
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    // Inscreva-se em notificações de mudança no carrinho
+    this.cartSubscription = this.cartService.onCartChanged().subscribe(() => {
+      // Atualize a variável finalizar quando o carrinho mudar
+      this.verificarCarrinho();
+    });
+
+    // Verifique o carrinho inicialmente
+    this.verificarCarrinho();
+  }
+
+  ngOnDestroy(): void {
+    // Certifique-se de cancelar a inscrição apenas se cartSubscription estiver definido
+    if (this.cartSubscription) {
+      this.cartSubscription.unsubscribe();
+    }
+  }
+
+  verificarCarrinho() {
+    // Verifica se há itens no carrinho
+    this.finalizar = this.cartService.getItems().length > 0;
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   private items: any[] = JSON.parse(localStorage.getItem('cartItem') || '[]')
+  private cartChangedSubject = new BehaviorSubject<void>(undefined);
 
   constructor(private router: Router) { }
   addToCart(product: any) {
-    const itemName = `Produto ${this.items.length + 1}`;
+    const itemName = `Açai ${this.items.length + 1}`;
     const price = typeof product.price === 'number' ? product.price : parseFloat(product.price) || 0;
-    this.items.push({ ...product, name: itemName, quantity: 1, price: price});
+    const total = price * 1; // Inicializa o total com o preço multiplicado pela quantidade (1)
+    this.items.push({ ...product, name: itemName, quantity: 1, price: price, total: total}); // Adiciona a propriedade total
     localStorage.setItem('cartItem', JSON.stringify(this.items));
     console.log('Itens no carrinho:', this.items);
-
-    this.router.navigateByUrl('/home')
-}
+    this.router.navigateByUrl('/home');
+  }
 
   getItems() {
     return this.items;}
@@ -33,6 +35,7 @@ export class CartService {
       const item = this.items.find((item) => item.id === id);
       if (item) {
         item.quantity++;
+        item.total = item.price * item.quantity; // Atualiza o total
         localStorage.setItem('cartItem', JSON.stringify(this.items));
       }
     }
@@ -41,8 +44,9 @@ export class CartService {
       const itemIndex = this.items.findIndex((item) => item.id === id);
       if (itemIndex !== -1 && this.items[itemIndex].quantity > 0) {
         this.items[itemIndex].quantity--;
+        this.items[itemIndex].total = this.items[itemIndex].price * this.items[itemIndex].quantity; // Atualiza o total
         if (this.items[itemIndex].quantity === 0) {
-          this.items.splice(itemIndex, 1); // Remove o item do array quando a quantidade for zero
+          this.items.splice(itemIndex, 1);
         }
         localStorage.setItem('cartItem', JSON.stringify(this.items));
       }
@@ -62,14 +66,15 @@ export class CartService {
     getItemsMessage(): string {
       let message = 'Itens no carrinho:\n';
       this.items.forEach((item, index) => {
-        message += `Produto ${index + 1}\n`;
+        message += `Açai ${index + 1}\n`;
         message += `Tipo: ${item.tipo}\n`;
         message += `Preço: ${item.price ? 'R$' + item.price.toFixed(2) : 'Preço não disponível'}\n`;
         message += `Creme: ${item.creme}\n`;
         message += `Fruta: ${item.fruta}\n`;
         message += `Coberturas: ${item.coberturas.join(',')}\n`;
         message += `Adicionais: ${item.adicionais.join(',')}\n`;
-        message += `Quantidade: ${item.quantity}\n\n`;
+        message += `Quantidade: ${item.quantity}\n`;
+        message += `Total: R$${(item.total+2).toFixed(2)}\n\n`
       });
       return message;
   }
@@ -84,5 +89,12 @@ clearCart() {
   this.items = []; // Limpa o array de itens
   localStorage.removeItem('cartItem'); // Remove o item do carrinho do localStorage
 }
+notifyCartChanged() {
+  this.cartChangedSubject.next();
+}
 
+// Método para se inscrever em notificações de mudança no carrinho
+onCartChanged() {
+  return this.cartChangedSubject.asObservable();
+}
 }
